586249142a70c6148b2920ce749a2d914f4481e1
gudetama21 <vbook20@gmail.com>
2020-12-28 21:46:39 +0800

    Update Main.java

--- src/costco/Main.java
+++ src/costco/Main.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JDialog.setDefaultLookAndFeelDecorated(true);
 		JFrame login = new Login();
 		login.setVisible(true);
 		
